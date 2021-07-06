0277825278ac3132b77ba224857af69a7e097405
Shashank Jaiswal <75845145+shashank-jaiswal-cn@users.noreply.github.com>
2021-07-06 17:04:18 +0530

    dynamic styling

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@ class App extends Component{
   state = {
     count: 0
   }
-
   render(){
+    // let classes = "btn btn-";
+    // classes+= this.state.count ===0 ? "danger" :"success"
+    //convert above lines into classes by selecting and clicking right click and then click refractor and then "extract to method in class App"
+    let classes = this.newMethod();
     //babel will convert this jsx into React.createElement("div"). 
     return (
       <div>
-        <span>{this.formatCount()}</span>
-        <button className="btn btn-danger">Increate</button>
+        <span >{this.formatCount()}</span>
+        <button className={classes}>Increate</button>
       </div>
     )
+  }
+
+  newMethod() {
+    let classes = "btn btn-";
+    classes += this.state.count === 0 ? "danger" : "success";
+    return classes;
   }
 
   formatCount(){
